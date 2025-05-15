7cedd7f9612949ade7232064a1d2c0b358f96e17
Seyibamzee <okalabi@cps.edu>
2025-05-15 14:10:20 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// 1. Covert to an arrow function.
+const sayHello = () => {
+    console.log("Hello, world!");
+  }
+
+
+// 2. Convert to an arrow function.
+const square = (num) => {
+    return num * num;
+  }
+  
+
+// 3. Convert to an arrow function.
+const addNums = (a, b) => {
+    return a + b;
+  }
+  
+
+
+console.log(sayHello())  // should print "Hello, world!"
+console.log(square(2)) // should print 4
+console.log(addNums(6, 4)) // should print 10
