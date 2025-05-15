544e0050562803d0f8d70db7a77b956f034771dc
Seyibamzee <okalabi@cps.edu>
2025-05-15 14:27:04 +0000

    wregg

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-// 1. Covert to an arrow function.
-const sayHello = () => {
-    console.log("Hello, world!");
-  }
+let input = document.querySelector(".input");
+let clearButton = document.querySelector(".clear");
+let equalsButton = document.querySelector(".equals");
+let result = document.querySelector(".result");
 
 
-// 2. Convert to an arrow function.
-const square = (num) => {
-    return num * num;
-  }
+// 1. Convert to an arrow function.
+const calculateDoggieAge=(humanAge)=> {
+
+  // 2. Declare a doggieAge variable and set it equal to the humanAge parameter multiplied by 7.
   
 
-// 3. Convert to an arrow function.
-const addNums = (a, b) => {
-    return a + b;
-  }
+  // 3. Update the result div's inner html to display the dog's age. Use string interpolation to create the message: "Your doggie is _________ years old in human years!"
+
+  
+  
+}
+
+
+
+equalsButton.addEventListener("click", () => {
+
+  // 4. Declare a userInput variable and set it equal to the value of the input. This needs to be a number, not a string! Be sure to place that inside our Number() method!
   
 
+  // 5. Call the calculateDoggieAge function. Be sure to include the userInput variable as the parameter!
 
-console.log(sayHello())  // should print "Hello, world!"
-console.log(square(2)) // should print 4
-console.log(addNums(6, 4)) // should print 10
+
+})
+
+
+
+// DO NOT TOUCH THIS CODE
+clearButton.onclick = function() {
+  input.value = "";
+  result.innerHTML = "";
+
+}
