17345ba9f0d96679bcde4422967eab87cce52074
Seyibamzee <okalabi@cps.edu>
2025-05-19 13:44:42 +0000

    sdvbsbegeg

--- script.js
+++ script.js
@@ -6,19 +6,32 @@ let result = document.querySelector(".result");
 
 // 1. Convert to an arrow function.
 const calculateDoggieAge=(humanAge)=> {
+  const doggieAge=humanAge*7;
+  return doggieAge;
+};
+
+const showDoggieAge = (age)=> {
+  result.innerHTML=`Your doggie is ${age} years old in human years!`;
+};
+const humanYears=0;
+const dogYears= calculateDoggieAge(humanYears);
+showDoggieAge(dogYears);
 
   // 2. Declare a doggieAge variable and set it equal to the humanAge parameter multiplied by 7.
   
 
   // 3. Update the result div's inner html to display the dog's age. Use string interpolation to create the message: "Your doggie is _________ years old in human years!"
 
-  
-  
-}
+    
+
 
 
 
 equalsButton.addEventListener("click", () => {
+  const userInput=Number(input.value);
+  const doggieAge= calculateDoggieAge(userInput);
+  showDoggieAge(doggieAge);
+  });
 
   // 4. Declare a userInput variable and set it equal to the value of the input. This needs to be a number, not a string! Be sure to place that inside our Number() method!
   
@@ -26,7 +39,7 @@ equalsButton.addEventListener("click", () => {
   // 5. Call the calculateDoggieAge function. Be sure to include the userInput variable as the parameter!
 
 
-})
+
 
 
 
